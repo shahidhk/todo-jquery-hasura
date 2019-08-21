51bd7f9b25853ec2865d25fca3adf6013c86bd9a
Shahidh K Muhammed <shahidh@hasura.io>
2019-08-21 23:50:20 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo-jquery-hasura
+
+A Todo app using jQuery and Hasura.
---
+++ index.js
@@ -0,0 +1,6 @@
+// A $( document ).ready() block.
+// read more about this at https://learn.jquery.com/using-jquery-core/document-ready/
+$( document ).ready(function() {
+  console.log( "jQuery and the document is ready!" );
+  $('#title').html('This was from JavaScript')
+});
